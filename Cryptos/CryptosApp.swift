411dfe114d2123195cfdd4ceaf541966266a6aaa
Sergioaquiles <sergioaquiles@hotmail.com>
2021-08-21 17:55:16 -0300

    Initial Commit

---
+++ Cryptos/CryptosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptosApp.swift
+//  Cryptos
+//
+//  Created by Sergio Cardoso on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
